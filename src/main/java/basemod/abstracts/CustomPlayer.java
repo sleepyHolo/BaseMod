44fd82499120d1be10c177f0acbf73f2d90c396d
Davis Cook <daviscook447@gmail.com>
2018-03-03 00:45:29 -0500

    Cleanup CustomPlayer constructor

--- src/main/java/basemod/abstracts/CustomPlayer.java
+++ src/main/java/basemod/abstracts/CustomPlayer.java
@@ -3,6 +3,7 @@ package basemod.abstracts;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,26 +13,47 @@ import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 public abstract class CustomPlayer extends AbstractPlayer{
 	
-	protected static ArrayList<Texture> ENERGY_LAYERS = new ArrayList<Texture>();
-	protected static Texture ORB_VFX;
+	private ArrayList<Texture> energyLayers = new ArrayList<Texture>();
+	private Texture orbVfx;
 
-	public CustomPlayer(String name, PlayerClass playerClass) {
+	public CustomPlayer(String name, PlayerClass playerClass, String[] orbTextures, String orbVfxPath) {
 		super(name, playerClass);
 		
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER1);
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER2);
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER3);
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER4);
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER5);
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER6);
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER1D);
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER2D);
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER3D);
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER4D);
-		ENERGY_LAYERS.add(ImageMaster.ENERGY_RED_LAYER5D);
+		if (orbTextures == null || orbVfxPath == null) {
+			buildDefaultOrb();
+		} else {
+			buildCustomOrb(orbTextures, orbVfxPath);
+		}
+	}
+	
+	public Texture getOrbVfxTexture() {
+		return orbVfx;
+	}
+	
+	public static final String DEFAULT_ORB_VFX = "images/ui/topPanel/energyRedVFX.png";
+	
+	private void buildDefaultOrb() {
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER1);
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER2);
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER3);
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER4);
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER5);
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER6);
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER1D);
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER2D);
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER3D);
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER4D);
+		energyLayers.add(ImageMaster.ENERGY_RED_LAYER5D);
+
+		orbVfx = ImageMaster.loadImage(DEFAULT_ORB_VFX);
+	}
+	
+	private void buildCustomOrb(String[] orbTextures, String orbVfxPath) {
+		for(String texPath : orbTextures) {
+			energyLayers.add(new Texture(Gdx.files.internal(texPath)));
+		}
 		
-		ORB_VFX = ImageMaster.loadImage("images/ui/topPanel/energyRedVFX.png");
-		
+		orbVfx = new Texture(Gdx.files.internal(orbVfxPath));
 	}
 	
 	public void renderOrb(EnergyPanel panel, SpriteBatch sb) {
@@ -77,17 +99,17 @@ public abstract class CustomPlayer extends AbstractPlayer{
 			
 			sb.setColor(Color.WHITE);
 			
-		    sb.draw(ENERGY_LAYERS.get(0), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle1, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(0), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle1, 0, 0, 128, 128, false, false);
 		    
-		    sb.draw(ENERGY_LAYERS.get(1), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle2, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(1), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle2, 0, 0, 128, 128, false, false);
 		    
-		    sb.draw(ENERGY_LAYERS.get(2), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle3, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(2), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle3, 0, 0, 128, 128, false, false);
 		    
-		    sb.draw(ENERGY_LAYERS.get(3), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle4, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(3), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle4, 0, 0, 128, 128, false, false);
 		    
-		    sb.draw(ENERGY_LAYERS.get(4), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle5, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(4), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle5, 0, 0, 128, 128, false, false);
 		    
-		    sb.draw(ENERGY_LAYERS.get(5), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, 0.0F, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(5), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, 0.0F, 0, 0, 128, 128, false, false);
 		    
 		    
 		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
@@ -97,15 +119,11 @@ public abstract class CustomPlayer extends AbstractPlayer{
 	}
 
 	public void renderDisabledOrb(EnergyPanel panel, SpriteBatch sb) {
-		
-		Field orb_scale;
-		Field orb_angle_1;
-		Field orb_angle_2;
-		Field orb_angle_3;
-		Field orb_angle_4;
-		Field orb_angle_5;
+		Field orb_scale, orb_angle_1, orb_angle_2, orb_angle_3,
+				orb_angle_4, orb_angle_5;
 		
 		try {
+			// access private fields off the EnergyPanel
 			orb_scale = panel.getClass().getDeclaredField("ORB_IMG_SCALE");
 			orb_scale.setAccessible(true);
 			float ORB_IMG_SCALE = orb_scale.getFloat(panel);
@@ -135,26 +153,22 @@ public abstract class CustomPlayer extends AbstractPlayer{
 			
 			float angle5 = orb_angle_5.getFloat(panel);
 			
-			
+			// actual rendering code
 			sb.setColor(Color.WHITE);
-		    sb.draw(ENERGY_LAYERS.get(6), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle1, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(6), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle1, 0, 0, 128, 128, false, false);
 		    
-		    sb.draw(ENERGY_LAYERS.get(7), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle2, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(7), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle2, 0, 0, 128, 128, false, false);
 		    
-		    sb.draw(ENERGY_LAYERS.get(8), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle3, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(8), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle3, 0, 0, 128, 128, false, false);
 		    
-		    sb.draw(ENERGY_LAYERS.get(9), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle4, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(9), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle4, 0, 0, 128, 128, false, false);
 		    
-		    sb.draw(ENERGY_LAYERS.get(10), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle5, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(10), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, angle5, 0, 0, 128, 128, false, false);
 		    
-		    sb.draw(ENERGY_LAYERS.get(5), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, 0.0F, 0, 0, 128, 128, false, false);
+		    sb.draw(energyLayers.get(5), panel.current_x - 64.0F, panel.current_y - 64.0F, 64.0F, 64.0F, 128.0F, 128.0F, ORB_IMG_SCALE, ORB_IMG_SCALE, 0.0F, 0, 0, 128, 128, false, false);
 		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
 			e.printStackTrace();
 		}
 		
-	}
-	
-	public Texture getOrbVfxTexture() {
-		return ORB_VFX;
 	}
 }
