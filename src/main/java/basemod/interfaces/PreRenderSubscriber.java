cb094ac0cc1efa05d53f92c972c64f23dbd23268
Davis Cook <daviscook447@gmail.com>
2018-03-03 14:59:29 -0500

    Remove unused imports

--- src/main/java/basemod/interfaces/PreRenderSubscriber.java
+++ src/main/java/basemod/interfaces/PreRenderSubscriber.java
@@ -1,7 +1,6 @@
 package basemod.interfaces;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public interface PreRenderSubscriber {
 	void receiveCameraRender(OrthographicCamera camera);
