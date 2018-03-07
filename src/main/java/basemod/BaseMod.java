e3f75eac4e172600c90f43242bc543f6f50b6da9
Davis Cook <daviscook447@gmail.com>
2018-03-07 18:47:41 -0500

    Only spend GPU time rendering the animation texture if necessary
    It will no longer waste GPU time if the number of model render
    subscribers is 0 or less

--- src/main/java/basemod/BaseMod.java
+++ src/main/java/basemod/BaseMod.java
@@ -1638,14 +1638,16 @@ public class BaseMod {
 	
 	// publishAnimationRender -
 	public static void publishAnimationRender(SpriteBatch sb) {
-		// custom animations
-		sb.end();
-		CardCrawlGame.psb.begin();
-		CardCrawlGame.psb.setBlendFunction(GL20.GL_ONE, GL20.GL_ONE_MINUS_SRC_ALPHA);
-		CardCrawlGame.psb.draw(animationTextureRegion, 0, 0);
-		CardCrawlGame.psb.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        CardCrawlGame.psb.end();
-        sb.begin();
+		if (modelRenderSubscribers.size() > 0) {
+			// custom animations
+			sb.end();
+			CardCrawlGame.psb.begin();
+			CardCrawlGame.psb.setBlendFunction(GL20.GL_ONE, GL20.GL_ONE_MINUS_SRC_ALPHA);
+			CardCrawlGame.psb.draw(animationTextureRegion, 0, 0);
+			CardCrawlGame.psb.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+	        CardCrawlGame.psb.end();
+	        sb.begin();
+		}
 	}
 	
 	// publishPreRender -
@@ -1654,21 +1656,23 @@ public class BaseMod {
 			sub.receiveCameraRender(camera);
 		}
 		
-		// custom animations
-		animationBuffer.begin();
-	    Gdx.gl.glClearColor(0f, 0f, 0f, 0f);
-	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	    batch.begin(animationCamera);
-	    
-	    for (ModelRenderSubscriber sub : modelRenderSubscribers) {
-	    	sub.receiveModelRender(batch, animationEnvironment);
-	    }
-	    
-	    batch.end();
-	    animationBuffer.end();
-        animationTexture = animationBuffer.getColorBufferTexture();
-        animationTextureRegion = new TextureRegion(animationTexture);
-        animationTextureRegion.flip(false, true);
+		if (modelRenderSubscribers.size() > 0) {
+			// custom animations
+			animationBuffer.begin();
+		    Gdx.gl.glClearColor(0f, 0f, 0f, 0f);
+		    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		    batch.begin(animationCamera);
+		    
+		    for (ModelRenderSubscriber sub : modelRenderSubscribers) {
+		    	sub.receiveModelRender(batch, animationEnvironment);
+		    }
+		    
+		    batch.end();
+		    animationBuffer.end();
+	        animationTexture = animationBuffer.getColorBufferTexture();
+	        animationTextureRegion = new TextureRegion(animationTexture);
+	        animationTextureRegion.flip(false, true);
+		}
 	}
 
 	// publishPostRender -
