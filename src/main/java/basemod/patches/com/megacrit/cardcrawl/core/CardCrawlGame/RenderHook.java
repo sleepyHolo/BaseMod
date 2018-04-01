d154a201e3b09d45f639c30955b556d24fb6bb06
Kiooeht <kiooeht@gmail.com>
2018-03-31 18:39:59 -0700

    Convert test Locators to new format

--- src/main/java/basemod/patches/com/megacrit/cardcrawl/core/CardCrawlGame/RenderHook.java
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/core/CardCrawlGame/RenderHook.java
@@ -3,10 +3,7 @@ package basemod.patches.com.megacrit.cardcrawl.core.CardCrawlGame;
 import java.util.ArrayList;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.evacipated.cardcrawl.modthespire.lib.LineFinder;
-import com.evacipated.cardcrawl.modthespire.lib.Matcher;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.helpers.DrawMaster;
 
@@ -16,16 +13,20 @@ import javassist.CtBehavior;
 
 @SpirePatch(cls = "com.megacrit.cardcrawl.core.CardCrawlGame", method = "render")
 public class RenderHook {
-	
+
 	@SpireInsertPatch(localvars = { "sb" })
 	public static void Insert(Object __obj_instance, SpriteBatch sb) {
 		BaseMod.publishRender(sb);
 	}
-	
-	public static int[] Locator(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
-		Matcher finalMatcher = new Matcher.MethodCallMatcher(DrawMaster.class.getName(), "draw");
-		
-		return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<Matcher>(), finalMatcher);
+
+	public static class Locator extends SpireInsertLocator
+	{
+		public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException
+		{
+			Matcher finalMatcher = new Matcher.MethodCallMatcher(DrawMaster.class.getName(), "draw");
+
+			return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<Matcher>(), finalMatcher);
+		}
 	}
 	
 }
