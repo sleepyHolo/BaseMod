f3ce4469d00f986fe7cd73ed716e70a6939f3ff6
Kiooeht <kiooeht@gmail.com>
2018-04-03 18:07:32 -0700

    Make long dynamic variables work on single card view

--- src/main/java/basemod/patches/com/megacrit/cardcrawl/screens/SingleCardViewPopup/RenderCustomDynamicVariable.java
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/screens/SingleCardViewPopup/RenderCustomDynamicVariable.java
@@ -11,22 +11,54 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.screens.SingleCardViewPopup;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Field;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpirePatch(
         cls="com.megacrit.cardcrawl.screens.SingleCardViewPopup",
-        method="renderDynamicVariable"
+        method="renderDescription"
 )
 public class RenderCustomDynamicVariable
 {
     private static Logger logger = LogManager.getLogger();
     private static final GlyphLayout gl = new GlyphLayout();
 
-    public static float Replace(SingleCardViewPopup __instance, char key, float start_x, float draw_y, int i, BitmapFont font, SpriteBatch sb, Character end)
+    public static void Raw(CtBehavior ctMethodToPatch) throws CannotCompileException
     {
+        final int[] insertLine = {-1};
+        ctMethodToPatch.instrument(new ExprEditor() {
+            @Override
+            public void edit(MethodCall m) throws CannotCompileException
+            {
+                if (m.getMethodName().equals("renderDynamicVariable")) {
+                    if (insertLine[0] == -1) {
+                        insertLine[0] = m.getLineNumber();
+                    }
+                    m.replace("$_ = basemod.patches.com.megacrit.cardcrawl.screens.SingleCardViewPopup.RenderCustomDynamicVariable.myRenderDynamicVariable(this, tmp, $$);");
+                }
+            }
+        });
+
+        if (insertLine[0] > 0) {
+            ctMethodToPatch.insertAt(insertLine[0]-1,
+                    "if (tmp.length() != 4 && tmp.length() != 5) {" +
+                            "start_x += basemod.patches.com.megacrit.cardcrawl.screens.SingleCardViewPopup.RenderCustomDynamicVariable.myRenderDynamicVariable(this, tmp, tmp.charAt(1), start_x, draw_y, i, font, sb, null);" +
+                            "}");
+        }
+    }
+
+    public static float myRenderDynamicVariable(Object __obj_instance, String key, char ckey, float start_x, float draw_y, int i, BitmapFont font, SpriteBatch sb, Character cend)
+    {
+        SingleCardViewPopup __instance = (SingleCardViewPopup)__obj_instance;
+
         // Get any private variables we need
         AbstractCard card;
         float current_x;
@@ -48,11 +80,20 @@ public class RenderCustomDynamicVariable
             return 0;
         }
 
+        String end = "";
+
+        Pattern pattern = Pattern.compile("!(.+)!(.*) ");
+        Matcher matcher = pattern.matcher(key);
+        if (matcher.find()) {
+            key = matcher.group(1);
+            end = matcher.group(2);
+        }
+
         // Main body of method
         StringBuilder stringBuilder = new StringBuilder();
         Color c = Settings.CREAM_COLOR;
         int num = 0;
-        DynamicVariable dv = BaseMod.cardDynamicVariableMap.get(Character.toString(key));
+        DynamicVariable dv = BaseMod.cardDynamicVariableMap.get(key);
         if (dv != null) {
             num = dv.baseValue(card);
             if (dv.upgraded(card)) {
@@ -69,7 +110,7 @@ public class RenderCustomDynamicVariable
                 i * 1.53f * -font.getCapHeight() + draw_y - current_y + -12.0f,
                 0.0f, true, c);
         if (end != null) {
-            FontHelper.renderRotatedText(sb, font, Character.toString(end),
+            FontHelper.renderRotatedText(sb, font, end,
                     current_x, current_y,
                     start_x - current_x + gl.width + 10.0f * Settings.scale,
                     i * 1.53f * -font.getCapHeight() + draw_y - current_y + -12.0f,
