a3a6afd0e4ce6f8a5cf648ac36e0d014cb440a58
Kiooeht <kiooeht@gmail.com>
2018-04-25 13:51:52 -0700

    Fix character select screen for mod ID on relics

---
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/screens/CharSelectInfo/RelicIDModID.java
@@ -0,0 +1,43 @@
+package basemod.patches.com.megacrit.cardcrawl.screens.CharSelectInfo;
+
+import basemod.BaseMod;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.relics.Circlet;
+import com.megacrit.cardcrawl.screens.CharSelectInfo;
+
+import java.util.ArrayList;
+
+@SpirePatch(
+        cls="com.megacrit.cardcrawl.screens.CharSelectInfo",
+        method="ctor",
+        paramtypes={"java.lang.String",
+                "java.lang.String",
+                "int",
+                "int",
+                "int",
+                "int",
+                "com.megacrit.cardcrawl.characters.AbstractPlayer$PlayerClass",
+                "java.util.ArrayList",
+                "java.util.ArrayList",
+                "boolean"
+        }
+)
+public class RelicIDModID
+{
+    public static void Prefix(CharSelectInfo __instance, String name, String flavorText, int currentHp, int maxHp, int gold, int cardDraw,
+                              AbstractPlayer.PlayerClass color, ArrayList<String> relics, ArrayList<String> deck, boolean resumeGame)
+    {
+        String modName = null;
+        for (int i=0; i<relics.size(); ++i) {
+            if (!relics.get(i).equals(Circlet.ID)) {
+                if (modName == null) {
+                    modName = BaseMod.findCallingModName();
+                }
+                if (modName != null && !relics.get(i).startsWith(modName + ":")) {
+                    relics.set(i, modName + ":" + relics.get(i));
+                }
+            }
+        }
+    }
+}
