69ea58eb5b7902e9afc113ac3376ce59ac2c53ea
Kiooeht <kiooeht@gmail.com>
2018-05-03 19:30:01 -0700

    Fix for week 23 AbstractCard changes

--- src/main/java/basemod/abstracts/CustomCard.java
+++ src/main/java/basemod/abstracts/CustomCard.java
@@ -62,10 +62,15 @@ public abstract class CustomCard extends AbstractCard {
 	public String textureBackgroundLargeImg = null;
 	public String textureBannerSmallImg = null;
 	public String textureBannerLargeImg = null;
-	
-	
+
+
+	@Deprecated
 	public CustomCard(String id, String name, String img, int cost, String rawDescription, CardType type, CardColor color, CardRarity rarity, CardTarget target, int cardPool) {
-		super(id, name, "status/beta", "status/beta", cost, rawDescription, type, color, rarity, target, cardPool);
+		this(id, name, img, cost, rawDescription, type, color, rarity, target);
+	}
+	
+	public CustomCard(String id, String name, String img, int cost, String rawDescription, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
+		super(id, name, "status/beta", "status/beta", cost, rawDescription, type, color, rarity, target);
 		
 		this.textureImg = img;
 		if (img != null) {
--- src/main/java/basemod/patches/com/megacrit/cardcrawl/cards/AbstractCard/CardIDModID.java
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/cards/AbstractCard/CardIDModID.java
@@ -20,15 +20,14 @@ import com.megacrit.cardcrawl.cards.DamageInfo;
                 "com.megacrit.cardcrawl.cards.AbstractCard$CardColor",
                 "com.megacrit.cardcrawl.cards.AbstractCard$CardRarity",
                 "com.megacrit.cardcrawl.cards.AbstractCard$CardTarget",
-                "com.megacrit.cardcrawl.cards.DamageInfo$DamageType",
-                "int"
+                "com.megacrit.cardcrawl.cards.DamageInfo$DamageType"
         }
 )
 public class CardIDModID
 {
     public static void Prefix(AbstractCard __instance, @ByRef String[] id, String name, String jokeUrl, String imgUrl, int cost, String rawDescription,
                               AbstractCard.CardType type, AbstractCard.CardColor color, AbstractCard.CardRarity rarity, AbstractCard.CardTarget target,
-                              DamageInfo.DamageType dType, int cardPool)
+                              DamageInfo.DamageType dType)
     {
         String modName = BaseMod.findCallingModName();
         if (modName != null && !id[0].startsWith(modName + ":")) {
