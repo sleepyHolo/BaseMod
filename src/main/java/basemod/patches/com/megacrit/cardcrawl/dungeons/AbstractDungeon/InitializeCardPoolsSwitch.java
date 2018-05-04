9b83cf941c36174bbd86c657277215ec5430d9ee
Kiooeht <kiooeht@gmail.com>
2018-05-03 20:06:03 -0700

    Fix initialize card pools for week 23

--- src/main/java/basemod/patches/com/megacrit/cardcrawl/dungeons/AbstractDungeon/InitializeCardPoolsSwitch.java
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/dungeons/AbstractDungeon/InitializeCardPoolsSwitch.java
@@ -3,8 +3,7 @@ package basemod.patches.com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import java.util.ArrayList;
 import java.util.Map;
 
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
@@ -13,11 +12,12 @@ import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.unlock.UnlockTracker;
 
 import basemod.BaseMod;
+import javassist.CtBehavior;
 
 @SpirePatch(cls="com.megacrit.cardcrawl.dungeons.AbstractDungeon", method="initializeCardPools")
 public class InitializeCardPoolsSwitch {
 
-	@SpireInsertPatch(rloc=26, localvars={"tmpPool"})
+	@SpireInsertPatch(localvars={"tmpPool"})
 	public static void Insert(Object __obj_instance, Object tmpPoolObj) {
 		AbstractPlayer player = AbstractDungeon.player;
 		AbstractPlayer.PlayerClass chosenClass = player.chosenClass;
@@ -35,5 +35,15 @@ public class InitializeCardPoolsSwitch {
 			}
 		}
 	}
-	
+
+	public static class Locator extends SpireInsertLocator {
+		@Override
+		public int[] Locate(CtBehavior ctBehavior) throws Exception
+		{
+			Matcher finalMatcher = new Matcher.MethodCallMatcher(
+					"com.megacrit.cardcrawl.dungeons.AbstractDungeon", "addColorlessCards");
+
+			return LineFinder.findInOrder(ctBehavior, new ArrayList<>(), finalMatcher);
+		}
+	}
 }
