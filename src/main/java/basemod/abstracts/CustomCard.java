c8b03b99a006c1eee76c7690f38785c5cd5e9bc7
GraysonnG <xinflux@gmail.com>
2018-05-04 08:03:56 -0400

    Fix Custom Card and ModalChoice Card to No longer use int cardPool
    Created a constructor that still contains cardPool but Deprecated it so
    we may eventually remove it.

--- src/main/java/basemod/abstracts/CustomCard.java
+++ src/main/java/basemod/abstracts/CustomCard.java
@@ -63,9 +63,13 @@ public abstract class CustomCard extends AbstractCard {
 	public String textureBannerSmallImg = null;
 	public String textureBannerLargeImg = null;
 	
-	
+	@Deprecated
 	public CustomCard(String id, String name, String img, int cost, String rawDescription, CardType type, CardColor color, CardRarity rarity, CardTarget target, int cardPool) {
-		super(id, name, "status/beta", "status/beta", cost, rawDescription, type, color, rarity, target, cardPool);
+		this(id, name, img, cost, rawDescription, type, color, rarity, target);
+	}
+	
+	public CustomCard(String id, String name, String img, int cost, String rawDescription, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
+		super(id, name, "status/beta", "status/beta", cost, rawDescription, type, color, rarity, target);
 		
 		this.textureImg = img;
 		if (img != null) {
