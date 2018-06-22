d5100acbd304de0f57cf54fb64f2f01f525082ca
Kiooeht <kiooeht@gmail.com>
2018-06-21 17:31:29 -0700

    Deprecate all the starting deck and relic hooks
    In preparation for merging the breaking changes to these hooks

--- src/main/java/basemod/interfaces/PostCreateStartingDeckSubscriber.java
+++ src/main/java/basemod/interfaces/PostCreateStartingDeckSubscriber.java
@@ -3,5 +3,6 @@ package basemod.interfaces;
 import java.util.ArrayList;
 
 public interface PostCreateStartingDeckSubscriber extends ISubscriber {
+	@Deprecated
 	boolean receivePostCreateStartingDeck(ArrayList<String> addCardsToMe);
 }
