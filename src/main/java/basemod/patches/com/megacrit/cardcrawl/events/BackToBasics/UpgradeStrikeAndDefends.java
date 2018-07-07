e975ca93338f817e805c7b13d16d29448a00184d
Twan van Laarhoven <twanvl@gmail.com>
2018-07-08 01:10:42 +0200

    Patch events and relics that care about Strikes and Defends.
    Mods can now declare a card to be a Strike by overriding isStrike() to return true, and similarly for isDefend(). The patches fix Ancient Writing, Vampires and Pandora's Box.

---
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/events/BackToBasics/UpgradeStrikeAndDefends.java
@@ -0,0 +1,27 @@
+package basemod.patches.com.megacrit.cardcrawl.events.BackToBasics;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardBrieflyEffect;
+
+import basemod.abstracts.CustomCard;
+
+@SpirePatch(cls = "com.megacrit.cardcrawl.events.thecity.BackToBasics", method = "upgradeStrikeAndDefends")
+public class UpgradeStrikeAndDefends {
+	@SpireInsertPatch(rloc = 30)
+	public static void Insert(Object __obj_instance) {
+		for (AbstractCard c : AbstractDungeon.player.masterDeck.group) {
+			if (c instanceof CustomCard && (((CustomCard)c).isStrike() || ((CustomCard)c).isDefend()) && c.canUpgrade()) {
+				c.upgrade();
+				AbstractDungeon.player.bottledCardUpgradeCheck(c);
+				AbstractDungeon.effectList.add(
+					new ShowCardBrieflyEffect(c.makeStatEquivalentCopy(), MathUtils.random(0.1F, 0.9F) * Settings.WIDTH,
+					MathUtils.random(0.2F, 0.8F) * Settings.HEIGHT));
+			}
+		}
+	}
+}
