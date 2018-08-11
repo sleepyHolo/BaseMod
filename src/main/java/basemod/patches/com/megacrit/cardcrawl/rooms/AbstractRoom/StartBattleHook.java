a33c953ffed89d34381e95ebfbd777b80409df40
Gogo <gogo81745.moe@gmail.com>
2018-08-11 20:17:06 +0800

    Fix StartBattleHook is unavailable after save and reopen game

--- src/main/java/basemod/patches/com/megacrit/cardcrawl/rooms/AbstractRoom/StartBattleHook.java
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/rooms/AbstractRoom/StartBattleHook.java
@@ -1,17 +1,20 @@
 package basemod.patches.com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import basemod.BaseMod;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.rooms.MonsterRoom;
 
 @SpirePatch(
-        cls = "com.megacrit.cardcrawl.rooms.MonsterRoom",
-        method = "onPlayerEntry"
+        cls = "com.megacrit.cardcrawl.rooms.AbstractRoom",
+        method = "update"
 )
 public class StartBattleHook {
 
-    public static void Prefix(MonsterRoom __instance) {
-        BaseMod.publishStartBattle(__instance);
+    @SpireInsertPatch(rloc = 44)
+    public static void Insert(AbstractRoom __instance) {
+        BaseMod.publishStartBattle((MonsterRoom) __instance);
     }
 
 }
