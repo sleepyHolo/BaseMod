b0fcd92de3179d63b147dffb079e4f46239f84a6
Kiooeht <kiooeht@gmail.com>
2018-08-18 23:29:51 -0700

    Make StartBattleHook more stable

--- src/main/java/basemod/patches/com/megacrit/cardcrawl/rooms/AbstractRoom/StartBattleHook.java
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/rooms/AbstractRoom/StartBattleHook.java
@@ -1,10 +1,10 @@
 package basemod.patches.com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import basemod.BaseMod;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.rooms.MonsterRoom;
+import javassist.CtBehavior;
 
 @SpirePatch(
         cls = "com.megacrit.cardcrawl.rooms.AbstractRoom",
@@ -12,9 +12,16 @@ import com.megacrit.cardcrawl.rooms.MonsterRoom;
 )
 public class StartBattleHook {
 
-    @SpireInsertPatch(rloc = 44)
+    @SpireInsertPatch()
     public static void Insert(AbstractRoom __instance) {
         BaseMod.publishStartBattle((MonsterRoom) __instance);
     }
 
+    public static class Locator extends SpireInsertLocator {
+        @Override
+        public int[] Locate(CtBehavior ctBehavior) throws Exception {
+            Matcher finalMatcher = new Matcher.MethodCallMatcher("com.megacrit.cardcrawl.characters.AbstractPlayer", "applyStartOfCombatPreDrawLogic");
+            return LineFinder.findInOrder(ctBehavior, finalMatcher);
+        }
+    }
 }
