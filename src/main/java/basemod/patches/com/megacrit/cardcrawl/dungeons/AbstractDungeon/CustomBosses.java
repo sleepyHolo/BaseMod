4f69ecfc3ce7a1df53e202f2161a7d426e347cf1
Kiooeht <kiooeht@gmail.com>
2018-09-30 22:09:15 -0700

    Fix Ascension 20 double boss when custom bosses have been added

--- src/main/java/basemod/patches/com/megacrit/cardcrawl/dungeons/AbstractDungeon/CustomBosses.java
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/dungeons/AbstractDungeon/CustomBosses.java
@@ -1,33 +1,36 @@
 package basemod.patches.com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 import basemod.BaseMod;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.map.DungeonMap;
+import com.megacrit.cardcrawl.ui.buttons.ProceedButton;
 import javassist.CannotCompileException;
+import javassist.CtBehavior;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 public class CustomBosses
 {
 	@SpirePatch(
-			cls="com.megacrit.cardcrawl.dungeons.AbstractDungeon",
+			clz=AbstractDungeon.class,
 			method=SpirePatch.CONSTRUCTOR,
-			paramtypes={
-					"java.lang.String",
-					"java.lang.String",
-					"com.megacrit.cardcrawl.characters.AbstractPlayer",
-					"java.util.ArrayList"
+			paramtypez={
+					String.class,
+					String.class,
+					AbstractPlayer.class,
+					ArrayList.class
 			}
 	)
 	public static class AddBosses
 	{
-		public static ExprEditor Instrument ()
+		public static ExprEditor Instrument()
 		{
 			return new ExprEditor() {
 				@Override
@@ -35,34 +38,32 @@ public class CustomBosses
 					if (m.getMethodName().equals("initializeBoss")) {
 						m.replace("{" +
 								"$_ = $proceed($$);" +
-								"basemod.patches.com.megacrit.cardcrawl.dungeons.AbstractDungeon.CustomBosses.AddBosses.Nested.Do(this);" +
+								"basemod.patches.com.megacrit.cardcrawl.dungeons.AbstractDungeon.CustomBosses.AddBosses.Do(this);" +
 								"}");
 					}
 				}
 			};
 		}
 
-		public static class Nested {
-			public static void Do(AbstractDungeon dungeon) {
-				// Don't add custom bosses if player is still on guaranteed bosses due to not seeing all bosses
-				if (AbstractDungeon.bossList.size() == 1) {
-					return;
-				}
-
-				AbstractDungeon.bossList.addAll(BaseMod.getBossIDs(AbstractDungeon.id));
-				Collections.shuffle(AbstractDungeon.bossList, new java.util.Random(AbstractDungeon.monsterRng.randomLong()));
+		public static void Do(AbstractDungeon dungeon) {
+			// Don't add custom bosses if player is still on guaranteed bosses due to not seeing all bosses
+			if (AbstractDungeon.bossList.size() == 1) {
+				return;
 			}
+
+			AbstractDungeon.bossList.addAll(BaseMod.getBossIDs(AbstractDungeon.id));
+			Collections.shuffle(AbstractDungeon.bossList, new java.util.Random(AbstractDungeon.monsterRng.randomLong()));
 		}
 	}
 
 	@SpirePatch(
-			cls="com.megacrit.cardcrawl.dungeons.AbstractDungeon",
+			clz=AbstractDungeon.class,
 			method="setBoss"
 	)
 	public static class SetBossIcon
 	{
 		private static final Logger logger = LogManager.getLogger(AbstractDungeon.class.getName());
-		public static SpireReturn Prefix(AbstractDungeon __instance, String key)
+		public static SpireReturn<Void> Prefix(AbstractDungeon __instance, String key)
 		{
 			BaseMod.BossInfo bossInfo = BaseMod.getBossInfo(key);
 			if (bossInfo != null) {
@@ -70,10 +71,37 @@ public class CustomBosses
 				DungeonMap.boss = bossInfo.bossMap;
 				DungeonMap.bossOutline = bossInfo.bossMapOutline;
 
-				logger.info("[BOSS] " + AbstractDungeon.bossList.get(0));
+				logger.info("[BOSS] " + key);
 				return SpireReturn.Return(null);
 			}
 			return SpireReturn.Continue();
+		}
+	}
+
+	@SpirePatch(
+			clz=ProceedButton.class,
+			method="update"
+	)
+	public static class Ascension20DoubleBoss
+	{
+		@SpireInsertPatch(
+				locator=Locator.class
+		)
+		public static void Insert(ProceedButton __instance)
+		{
+			while (AbstractDungeon.bossList.size() > 2) {
+				AbstractDungeon.bossList.remove(AbstractDungeon.bossList.size() - 1);
+			}
+		}
+
+		private static class Locator extends SpireInsertLocator
+		{
+			@Override
+			public int[] Locate(CtBehavior ctBehavior) throws Exception
+			{
+				Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractDungeon.class, "ascensionLevel");
+				return LineFinder.findInOrder(ctBehavior, finalMatcher);
+			}
 		}
 	}
 }
