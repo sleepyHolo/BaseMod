0a9c6340555cde075ebe6c93d4e3c17b758a6baf
Kiooeht <kiooeht@gmail.com>
2018-10-12 23:04:08 -0700

    Fix receivePostDraw getting called for every relic

--- src/main/java/basemod/patches/com/megacrit/cardcrawl/characters/AbstractPlayer/PostDrawHook.java
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/characters/AbstractPlayer/PostDrawHook.java
@@ -1,15 +1,36 @@
 package basemod.patches.com.megacrit.cardcrawl.characters.AbstractPlayer;
 
 import basemod.BaseMod;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import javassist.CtBehavior;
 
-@SpirePatch(cls="com.megacrit.cardcrawl.characters.AbstractPlayer", method="draw", paramtypes={"int"})
-public class PostDrawHook {
-    @SpireInsertPatch(rloc=31, localvars={"c"})
-    public static void Insert(Object mObj, int numCards, Object cObj) {
-    	AbstractCard c = (AbstractCard) cObj;
+@SpirePatch(
+		clz=AbstractPlayer.class,
+		method="draw",
+		paramtypez={
+				int.class
+		}
+)
+public class PostDrawHook
+{
+    @SpireInsertPatch(
+    		locator=Locator.class,
+			localvars={"c"}
+	)
+    public static void Insert(AbstractPlayer __instance, int numCards, AbstractCard c)
+	{
         BaseMod.publishPostDraw(c);
     }
+
+	private static class Locator extends SpireInsertLocator
+	{
+		@Override
+		public int[] Locate(CtBehavior ctMethodToPatch) throws Exception
+		{
+			Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractPlayer.class, "relics");
+			return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+		}
+	}
 }
