e9aa93eae34a599f25bda0164e5c908d4fa0ba63
Kiooeht <kiooeht@gmail.com>
2018-11-01 17:55:19 -0700

    Fix whatmod for events not obeying setting and appearing on Neow

--- src/main/java/basemod/patches/whatmod/Events.java
+++ src/main/java/basemod/patches/whatmod/Events.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.events.AbstractEvent;
+import com.megacrit.cardcrawl.neow.NeowEvent;
 
 @SpirePatch(
 		clz=AbstractEvent.class,
@@ -13,6 +14,8 @@ public class Events
 {
 	public static void Postfix(AbstractEvent __instance, SpriteBatch sb)
 	{
-		WhatMod.renderModTooltip(sb, __instance.getClass(), 1500.0f * Settings.scale, 900.0f * Settings.scale);
+		if (WhatMod.enabled && !(__instance instanceof NeowEvent)) {
+			WhatMod.renderModTooltip(sb, __instance.getClass(), 1500.0f * Settings.scale, 900.0f * Settings.scale);
+		}
 	}
 }
