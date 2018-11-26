4fefc5076f2ef12c1e75659aff685882248e0fc3
Kiooeht <kiooeht@gmail.com>
2018-11-25 17:14:51 -0800

    Fix Whatmod appearing on heart screen and in event combats

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -485,3 +485,4 @@ This is a fast forward to v1.6.3 of daviscook477's fork with a few additional ch
 * Fix max HP change hook triggering on ANY creature changing max HP (kiooeht)
 
 #### dev ####
+* Fix Whatmod appearing on heart screen and in event combats (kiooeht)
--- src/main/java/basemod/patches/whatmod/Events.java
+++ src/main/java/basemod/patches/whatmod/Events.java
@@ -3,8 +3,11 @@ package basemod.patches.whatmod;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.AbstractEvent;
+import com.megacrit.cardcrawl.events.beyond.SpireHeart;
 import com.megacrit.cardcrawl.neow.NeowEvent;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 @SpirePatch(
 		clz=AbstractEvent.class,
@@ -14,7 +17,10 @@ public class Events
 {
 	public static void Postfix(AbstractEvent __instance, SpriteBatch sb)
 	{
-		if (WhatMod.enabled && !(__instance instanceof NeowEvent)) {
+		if (WhatMod.enabled
+				&& !(__instance instanceof NeowEvent)
+				&& !(__instance instanceof SpireHeart)
+				&& AbstractDungeon.getCurrRoom().phase != AbstractRoom.RoomPhase.COMBAT) {
 			WhatMod.renderModTooltip(sb, __instance.getClass(), 1500.0f * Settings.scale, 900.0f * Settings.scale);
 		}
 	}
