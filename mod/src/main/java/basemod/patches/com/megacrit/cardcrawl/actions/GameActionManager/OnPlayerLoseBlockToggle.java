07299d2e5da8f0b6667d021ac78a93cfb12d7bfd
Kiooeht <kiooeht@gmail.com>
2018-12-21 18:20:18 -0800

    Cleanup

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/actions/GameActionManager/OnPlayerLoseBlockToggle.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/actions/GameActionManager/OnPlayerLoseBlockToggle.java
@@ -2,23 +2,53 @@ package basemod.patches.com.megacrit.cardcrawl.actions.GameActionManager;
 
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import javassist.CtBehavior;
 
-@SpirePatch(cls="com.megacrit.cardcrawl.actions.GameActionManager", method="getNextAction")
-public class OnPlayerLoseBlockToggle {
-	public static boolean on = false;
+import java.util.ArrayList;
+
+@SpirePatch(
+		clz=GameActionManager.class,
+		method="getNextAction"
+)
+public class OnPlayerLoseBlockToggle
+{
+	public static boolean isEnabled = false;
 	
 	@SpireInsertPatch(
-			rloc=197
+			locator=LocatorPre.class
 	)
-	public static void InsertPre(GameActionManager __instance) {
-		on = true;
+	public static void InsertPre(GameActionManager __instance)
+	{
+		isEnabled = true;
 	}
 
 	@SpireInsertPatch(
-			rloc=206
+			locator=LocatorPost.class
 	)
-	public static void InsertPost(GameActionManager __instance) {
-		on = false;
+	public static void InsertPost(GameActionManager __instance)
+	{
+		isEnabled = false;
 	}
 
+	private static class LocatorPre extends SpireInsertLocator
+	{
+		@Override
+		public int[] Locate(CtBehavior ctBehavior) throws Exception
+		{
+			Matcher matcher = new Matcher.MethodCallMatcher(AbstractPlayer.class, "hasPower");
+			return LineFinder.findInOrder(ctBehavior, matcher);
+		}
+	}
+
+	private static class LocatorPost extends SpireInsertLocator
+	{
+		@Override
+		public int[] Locate(CtBehavior ctBehavior) throws Exception
+		{
+			Matcher matcher = new Matcher.FieldAccessMatcher(AbstractRoom.class, "isBattleOver");
+			return LineFinder.findInOrder(ctBehavior, new ArrayList<>(), matcher);
+		}
+	}
 }
--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/core/AbstractCreature/ModifyPlayerLoseBlock.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/core/AbstractCreature/ModifyPlayerLoseBlock.java
@@ -3,29 +3,26 @@ package basemod.patches.com.megacrit.cardcrawl.core.AbstractCreature;
 import basemod.BaseMod;
 import basemod.patches.com.megacrit.cardcrawl.actions.GameActionManager.OnPlayerLoseBlockToggle;
 import com.evacipated.cardcrawl.modthespire.lib.ByRef;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 
 @SpirePatch(
-		clz= AbstractCreature.class,
+		clz=AbstractCreature.class,
 		method="loseBlock",
-		paramtypez= {
+		paramtypez={
 				int.class,
 				boolean.class
-		})
+		}
+)
 public class ModifyPlayerLoseBlock
 {
-	@SpireInsertPatch(
-			rloc = 0,
-			localvars = {"amount"}
-	)
-	public static void Insert(AbstractCreature __instance, int amount, boolean noAnimation, @ByRef int[] modifyAmount){
-		if (OnPlayerLoseBlockToggle.on) {
-			modifyAmount[0] = BaseMod.publishOnPlayerLoseBlock(amount);
+	public static void Prefix(AbstractCreature __instance, @ByRef int[] amount, boolean noAnimation)
+	{
+		if (OnPlayerLoseBlockToggle.isEnabled) {
+			amount[0] = BaseMod.publishOnPlayerLoseBlock(amount[0]);
 		}
-		if (modifyAmount[0] < 0) {
-			modifyAmount[0] = 0;
+		if (amount[0] < 0) {
+			amount[0] = 0;
 		}
 	}
 }
