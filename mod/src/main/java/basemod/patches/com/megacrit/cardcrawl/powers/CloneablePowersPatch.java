29968de44f795b039d2ef71d898a40807200a81f
reina <ReinaSHSL@users.noreply.github.com>
2019-02-14 17:18:13 -0800

    Shifting

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
@@ -2331,4 +2331,29 @@ public class CloneablePowersPatch {
             ctClass.addMethod(method);
         }
     }
+
+    @SpirePatch(
+            clz = ShiftingPower.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+    public static class ShiftingPowerPatch {
+        public static void Raw(CtBehavior ctMethodToPatch) throws NotFoundException, CannotCompileException {
+            CtClass ctClass = ctMethodToPatch.getDeclaringClass();
+            ClassPool pool = ctClass.getClassPool();
+
+            CtClass ctCloneablePowerInterface = pool.get(CloneablePowerInterface.class.getName());
+            ctClass.addInterface(ctCloneablePowerInterface);
+            CtClass ctAbstractPower = pool.get(AbstractPower.class.getName());
+
+            CtMethod method = CtNewMethod.make(
+                    ctAbstractPower, // Return
+                    "makeCopy", // Method name
+                    new CtClass[]{},
+                    null, // Exceptions
+                    "return new " + ShiftingPower.class.getName() + "(owner);",
+                    ctClass
+            );
+            ctClass.addMethod(method);
+        }
+    }
 }
