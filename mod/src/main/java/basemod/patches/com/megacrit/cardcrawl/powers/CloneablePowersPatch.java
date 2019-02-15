665ff3bff268227ec04c08ed606b288fd00cad95
reina <ReinaSHSL@users.noreply.github.com>
2019-02-14 16:48:06 -0800

    Double Damage

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
@@ -731,4 +731,29 @@ public class CloneablePowersPatch {
             ctClass.addMethod(method);
         }
     }
+
+    @SpirePatch(
+            clz = DoubleDamagePower.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+    public static class DoubleDamagePowerPatch {
+        public static void Raw(CtBehavior ctMethodToPatch) throws NotFoundException, CannotCompileException {
+            CtClass ctClass = ctMethodToPatch.getDeclaringClass();
+            ClassPool pool = ctClass.getClassPool();
+
+            CtClass ctCloneablePowerInterface = pool.get(CloneablePowerInterface.class.getName());
+            ctClass.addInterface(ctCloneablePowerInterface);
+            CtClass ctAbstractPower = pool.get(AbstractPower.class.getName());
+
+            CtMethod method = CtNewMethod.make(
+                    ctAbstractPower, // Return
+                    "makeCopy", // Method name
+                    new CtClass[]{},
+                    null, // Exceptions
+                    "return new " + DoubleDamagePower.class.getName() + "(owner, amount, justApplied);",
+                    ctClass
+            );
+            ctClass.addMethod(method);
+        }
+    }
 }
