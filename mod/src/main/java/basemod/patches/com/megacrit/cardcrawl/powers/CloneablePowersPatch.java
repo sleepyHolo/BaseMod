1bf419bae30ae7a0c7f26fbcf22dacf15db50b3c
reina <ReinaSHSL@users.noreply.github.com>
2019-02-14 17:10:54 -0800

    Nightmare

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
@@ -1781,4 +1781,29 @@ public class CloneablePowersPatch {
             ctClass.addMethod(method);
         }
     }
+
+    @SpirePatch(
+            clz = NightmarePower.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+    public static class NightmarePowerPatch {
+        public static void Raw(CtBehavior ctMethodToPatch) throws NotFoundException, CannotCompileException {
+            CtClass ctClass = ctMethodToPatch.getDeclaringClass();
+            ClassPool pool = ctClass.getClassPool();
+
+            CtClass ctCloneablePowerInterface = pool.get(CloneablePowerInterface.class.getName());
+            ctClass.addInterface(ctCloneablePowerInterface);
+            CtClass ctAbstractPower = pool.get(AbstractPower.class.getName());
+
+            CtMethod method = CtNewMethod.make(
+                    ctAbstractPower, // Return
+                    "makeCopy", // Method name
+                    new CtClass[]{},
+                    null, // Exceptions
+                    "return new " + NightmarePower.class.getName() + "(owner, amount, card);",
+                    ctClass
+            );
+            ctClass.addMethod(method);
+        }
+    }
 }
