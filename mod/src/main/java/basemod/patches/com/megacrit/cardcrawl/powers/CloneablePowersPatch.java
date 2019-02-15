095fbffe1abafb33135671034a13affce53e75ce
reina <ReinaSHSL@users.noreply.github.com>
2019-02-14 16:50:28 -0800

    Draw Card Next Turn

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
@@ -781,4 +781,29 @@ public class CloneablePowersPatch {
             ctClass.addMethod(method);
         }
     }
+    
+    @SpirePatch(
+    clz = DrawCardNextTurnPower.class,
+    method = SpirePatch.CONSTRUCTOR
+    )
+    public static class DrawCardNextTurnPowerPatch {
+        public static void Raw(CtBehavior ctMethodToPatch) throws NotFoundException, CannotCompileException {
+            CtClass ctClass = ctMethodToPatch.getDeclaringClass();
+            ClassPool pool = ctClass.getClassPool();
+
+            CtClass ctCloneablePowerInterface = pool.get(CloneablePowerInterface.class.getName());
+            ctClass.addInterface(ctCloneablePowerInterface);
+            CtClass ctAbstractPower = pool.get(AbstractPower.class.getName());
+
+            CtMethod method = CtNewMethod.make(
+                    ctAbstractPower, // Return
+                    "makeCopy", // Method name
+                    new CtClass[]{},
+                    null, // Exceptions
+                    "return new " + DrawCardNextTurnPower.class.getName() + "(owner, amount);",
+                    ctClass
+            );
+            ctClass.addMethod(method);
+        }
+    }
 }
