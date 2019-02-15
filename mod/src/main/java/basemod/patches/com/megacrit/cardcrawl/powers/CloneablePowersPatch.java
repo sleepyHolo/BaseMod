6cd7975ac83d37f372573e2ec660ea7922a57de0
reina <ReinaSHSL@users.noreply.github.com>
2019-02-14 17:24:02 -0800

    Weak

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
@@ -2806,4 +2806,29 @@ public class CloneablePowersPatch {
             ctClass.addMethod(method);
         }
     }
+
+    @SpirePatch(
+            clz = WeakPower.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+    public static class WeakPowerPatch {
+        public static void Raw(CtBehavior ctMethodToPatch) throws NotFoundException, CannotCompileException {
+            CtClass ctClass = ctMethodToPatch.getDeclaringClass();
+            ClassPool pool = ctClass.getClassPool();
+
+            CtClass ctCloneablePowerInterface = pool.get(CloneablePowerInterface.class.getName());
+            ctClass.addInterface(ctCloneablePowerInterface);
+            CtClass ctAbstractPower = pool.get(AbstractPower.class.getName());
+
+            CtMethod method = CtNewMethod.make(
+                    ctAbstractPower, // Return
+                    "makeCopy", // Method name
+                    new CtClass[]{},
+                    null, // Exceptions
+                    "return new " + WeakPower.class.getName() + "(owner, amount, justApplied);",
+                    ctClass
+            );
+            ctClass.addMethod(method);
+        }
+    }
 }
