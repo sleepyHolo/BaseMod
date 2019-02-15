f896e074cdf8600ef54595e69f60e23f28fc1fd2
reina <ReinaSHSL@users.noreply.github.com>
2019-02-14 16:32:29 -0800

    After Image

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
@@ -4,6 +4,7 @@ import basemod.interfaces.CloneablePowerInterface;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.AccuracyPower;
+import com.megacrit.cardcrawl.powers.AfterImagePower;
 import javassist.*;
 
 public class CloneablePowersPatch {
@@ -27,6 +28,31 @@ public class CloneablePowersPatch {
                     new CtClass[]{},
                     null, // Exceptions
                     "return new " + AccuracyPower.class.getName() + "(owner, amount);",
+                    ctClass
+            );
+            ctClass.addMethod(method);
+        }
+    }
+
+    @SpirePatch(
+            clz = AfterImagePower.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+    public static class AfterImagePowerPatch {
+        public static void Raw(CtBehavior ctMethodToPatch) throws NotFoundException, CannotCompileException {
+            CtClass ctClass = ctMethodToPatch.getDeclaringClass();
+            ClassPool pool = ctClass.getClassPool();
+
+            CtClass ctCloneablePowerInterface = pool.get(CloneablePowerInterface.class.getName());
+            ctClass.addInterface(ctCloneablePowerInterface);
+            CtClass ctAbstractPower = pool.get(AbstractPower.class.getName());
+
+            CtMethod method = CtNewMethod.make(
+                    ctAbstractPower, // Return
+                    "makeCopy", // Method name
+                    new CtClass[]{},
+                    null, // Exceptions
+                    "return new " + AfterImagePower.class.getName() + "(owner, amount);",
                     ctClass
             );
             ctClass.addMethod(method);
