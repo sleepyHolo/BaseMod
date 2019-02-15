8414ad0bd05460bde4d9a818852de85e9a48f388
reina <ReinaSHSL@users.noreply.github.com>
2019-02-14 17:17:03 -0800

    Retain Card

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
@@ -2206,4 +2206,29 @@ public class CloneablePowersPatch {
             ctClass.addMethod(method);
         }
     }
+
+    @SpirePatch(
+            clz = RetainCardPower.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+    public static class RetainCardPowerPatch {
+        public static void Raw(CtBehavior ctMethodToPatch) throws NotFoundException, CannotCompileException {
+            CtClass ctClass = ctMethodToPatch.getDeclaringClass();
+            ClassPool pool = ctClass.getClassPool();
+
+            CtClass ctCloneablePowerInterface = pool.get(CloneablePowerInterface.class.getName());
+            ctClass.addInterface(ctCloneablePowerInterface);
+            CtClass ctAbstractPower = pool.get(AbstractPower.class.getName());
+
+            CtMethod method = CtNewMethod.make(
+                    ctAbstractPower, // Return
+                    "makeCopy", // Method name
+                    new CtClass[]{},
+                    null, // Exceptions
+                    "return new " + RetainCardPower.class.getName() + "(owner, amount);",
+                    ctClass
+            );
+            ctClass.addMethod(method);
+        }
+    }
 }
