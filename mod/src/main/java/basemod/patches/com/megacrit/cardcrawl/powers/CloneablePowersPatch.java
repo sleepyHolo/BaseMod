b36cc5d5889e84cb5b9c64f2d3e248c3fa7f591d
reina <ReinaSHSL@users.noreply.github.com>
2019-02-14 17:01:50 -0800

    Intangible Player

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/powers/CloneablePowersPatch.java
@@ -1456,4 +1456,29 @@ public class CloneablePowersPatch {
             ctClass.addMethod(method);
         }
     }
+
+    @SpirePatch(
+            clz = IntangiblePlayerPower.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+    public static class IntangiblePlayerPowerPatch {
+        public static void Raw(CtBehavior ctMethodToPatch) throws NotFoundException, CannotCompileException {
+            CtClass ctClass = ctMethodToPatch.getDeclaringClass();
+            ClassPool pool = ctClass.getClassPool();
+
+            CtClass ctCloneablePowerInterface = pool.get(CloneablePowerInterface.class.getName());
+            ctClass.addInterface(ctCloneablePowerInterface);
+            CtClass ctAbstractPower = pool.get(AbstractPower.class.getName());
+
+            CtMethod method = CtNewMethod.make(
+                    ctAbstractPower, // Return
+                    "makeCopy", // Method name
+                    new CtClass[]{},
+                    null, // Exceptions
+                    "return new " + IntangiblePlayerPower.class.getName() + "(owner, amount);",
+                    ctClass
+            );
+            ctClass.addMethod(method);
+        }
+    }
 }
