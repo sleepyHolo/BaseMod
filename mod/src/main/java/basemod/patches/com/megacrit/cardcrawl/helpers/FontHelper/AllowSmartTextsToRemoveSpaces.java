0fcb590d7f6ec482fdc0cb7c5ec4313a754701f9
Kiooeht <kiooeht@gmail.com>
2019-02-25 19:57:52 -0800

    Fix incorrect package, this isn't blackbeard

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/helpers/FontHelper/AllowSmartTextsToRemoveSpaces.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/helpers/FontHelper/AllowSmartTextsToRemoveSpaces.java
@@ -1,4 +1,4 @@
-package blackbeard.patches;
+package basemod.patches.com.megacrit.cardcrawl.helpers.FontHelper;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
