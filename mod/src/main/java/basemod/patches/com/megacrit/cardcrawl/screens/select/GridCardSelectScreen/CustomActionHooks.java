de40aa6d37c19c7d080d93d8681651f4f1a9392f
Gremious <gremious@protonmail.com>
2019-05-21 12:04:44 +0100

    Changed rloc patches to locator patches. (#1)

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/screens/select/GridCardSelectScreen/CustomActionHooks.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/screens/select/GridCardSelectScreen/CustomActionHooks.java
@@ -1,41 +1,51 @@
 package basemod.patches.com.megacrit.cardcrawl.screens.select.GridCardSelectScreen;
 
 import basemod.BaseMod;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.screens.select.GridCardSelectScreen;
+import javassist.CtBehavior;
 
 import java.util.ArrayList;
 
 @SuppressWarnings("unused")
 public class CustomActionHooks {
-
+    
     @SuppressWarnings("unused")
     @SpirePatch(clz = GridCardSelectScreen.class, method = "update")
     public static class updatePatch {
-
-        @SpireInsertPatch(rloc = 61)
+        
+        @SpireInsertPatch(
+                locator = updateLocator.class
+        )
         public static void Insert(GridCardSelectScreen __instance) {
             if (GridCardSelectScreenFields.forCustomReward.get(__instance)) {
                 ArrayList<AbstractCard> selectedCards = __instance.selectedCards;
                 selectedCards.forEach(AbstractCard::stopGlowing);
-
+                
                 if (GridCardSelectScreenFields.customCallback.get(__instance) == null) {
                     BaseMod.logger.error("GridCardSelectScreen is being called for a custom reward without a callback");
                 }
-
+                
                 // Callback that was passed in from `open` is executed here
                 GridCardSelectScreenFields.customCallback.get(__instance).callback(selectedCards);
-
+                
                 // Reset screen
                 __instance.selectedCards.clear();
                 GridCardSelectScreenFields.resetFields(__instance);
             }
         }
     }
-
+    
+    private static class updateLocator extends SpireInsertLocator {
+        @Override
+        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+            Matcher finalMatcher = new Matcher.FieldAccessMatcher(GridCardSelectScreen.class, "forUpgrade");
+            return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+        }
+    }
+    
     @SuppressWarnings("unused")
     @SpirePatch(clz = GridCardSelectScreen.class, method = "reopen")
     public static class reopenPatch {
@@ -45,7 +55,7 @@ public class CustomActionHooks {
             }
         }
     }
-
+    
     @SuppressWarnings("unused")
     @SpirePatch(clz = GridCardSelectScreen.class, method = "callOnOpen")
     public static class resetFlagsOnOpen {
@@ -53,15 +63,25 @@ public class CustomActionHooks {
             GridCardSelectScreenFields.resetFields(__instance);
         }
     }
-
+    
     @SuppressWarnings("unused")
     @SpirePatch(clz = AbstractDungeon.class, method = "closeCurrentScreen")
     public static class resetFlagsOnClose {
-        @SpireInsertPatch(rloc = 51)
+        @SpireInsertPatch(
+                locator = closeCurrentScreenLocator.class
+        )
         public static void Insert() {
             if (GridCardSelectScreenFields.forCustomReward.get(AbstractDungeon.gridSelectScreen)) {
                 AbstractDungeon.gridSelectScreen.selectedCards.clear();
             }
+        }
+    }
+    
+    private static class closeCurrentScreenLocator extends SpireInsertLocator {
+        @Override
+        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+            Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractDungeon.class, "genericScreenOverlayReset");
+            return new int[]{LineFinder.findAllInOrder(ctMethodToPatch, finalMatcher)[8]};
         }
     }
 }
