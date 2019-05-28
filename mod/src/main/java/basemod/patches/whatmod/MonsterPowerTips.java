2f534a58981c82e9a176db28b5bbca1456f1d42f
Kiooeht <kiooeht@gmail.com>
2019-05-27 22:02:56 -0700

    Fix whatmod power tips for beta

--- mod/src/main/java/basemod/patches/whatmod/MonsterPowerTips.java
+++ mod/src/main/java/basemod/patches/whatmod/MonsterPowerTips.java
@@ -1,18 +1,18 @@
 package basemod.patches.whatmod;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import javassist.CannotCompileException;
+import javassist.CtBehavior;
 import javassist.expr.ExprEditor;
 import javassist.expr.NewExpr;
 
 @SpirePatch(
-    cls="com.megacrit.cardcrawl.monsters.AbstractMonster",
+    clz=AbstractMonster.class,
     method="renderTip"
 )
 public class MonsterPowerTips
@@ -20,12 +20,22 @@ public class MonsterPowerTips
     private static String modName = null;
 
     @SpireInsertPatch(
-        rloc=6,
+        locator=Locator.class,
         localvars={"p"}
     )
     public static void Insert(AbstractMonster __instance, SpriteBatch sb, AbstractPower power)
     {
         modName = WhatMod.findModName(power.getClass());
+    }
+
+    private static class Locator extends SpireInsertLocator
+    {
+        @Override
+        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception
+        {
+            Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractPower.class, "region48");
+            return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+        }
     }
 
     public static PowerTip alterTip(PowerTip tip)
@@ -37,7 +47,7 @@ public class MonsterPowerTips
     }
 
     @SpirePatch(
-        cls="com.megacrit.cardcrawl.monsters.AbstractMonster",
+		clz=AbstractMonster.class,
         method="renderTip"
     )
     public static class Nested
@@ -49,8 +59,8 @@ public class MonsterPowerTips
                 @Override
                 public void edit(NewExpr e) throws CannotCompileException
                 {
-                    if (e.getClassName().equals("com.megacrit.cardcrawl.helpers.PowerTip")) {
-                        e.replace("$_ = basemod.patches.whatmod.MonsterPowerTips.alterTip($proceed($$));");
+                    if (e.getClassName().equals(PowerTip.class.getName())) {
+                        e.replace(String.format("$_ = %s.alterTip($proceed($$));", MonsterPowerTips.class.getName()));
                     }
                 }
             };
