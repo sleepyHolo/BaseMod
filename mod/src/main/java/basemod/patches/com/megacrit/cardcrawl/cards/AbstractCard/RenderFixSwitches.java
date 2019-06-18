32c242600a33ff8d98e723fff75a78dce14e5eaa
Kiooeht <kiooeht@gmail.com>
2019-06-17 22:21:41 -0700

    Fix custom card rendering for beta

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/cards/AbstractCard/RenderFixSwitches.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/cards/AbstractCard/RenderFixSwitches.java
@@ -1,23 +1,21 @@
 package basemod.patches.com.megacrit.cardcrawl.cards.AbstractCard;
 
 import basemod.BaseMod;
-import basemod.ReflectionHacks;
 import basemod.abstracts.CustomCard;
 import basemod.helpers.SuperclassFinder;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardColor;
-import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
-import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import javassist.CannotCompileException;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,121 +23,105 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class RenderFixSwitches {
-
-	@SpirePatch(cls = "com.megacrit.cardcrawl.cards.AbstractCard", method = "renderBannerImage")
-	public static class RenderBannerSwitch {
+public class RenderFixSwitches
+{
+	@SpirePatch(
+			clz=AbstractCard.class,
+			method="renderBannerImage"
+	)
+	public static class RenderBannerSwitch
+	{
 		public static final Logger logger = LogManager.getLogger(BaseMod.class.getName());
 		
-		public static SpireReturn<?> Prefix(AbstractCard __instance, SpriteBatch sb, float drawX, float drawY) {
+		public static SpireReturn<?> Prefix(AbstractCard __instance, SpriteBatch sb, float drawX, float drawY)
+		{
 			//if it is not a custom card it cant possibly have the method getBannerSmallTexture force the normal rendering
-			if(!(__instance instanceof CustomCard)) return SpireReturn.Continue();
+			if (!(__instance instanceof CustomCard)) {
+				return SpireReturn.Continue();
+			}
 			
 			CustomCard card = (CustomCard) __instance;
 			Texture texture = card.getBannerSmallTexture();
-			if(texture == null) {
+			if (texture == null) {
 				return SpireReturn.Continue();
 			}
 			
-			renderHelper(card, sb, Color.WHITE, texture, drawX, drawY);
+			renderHelper(card, sb, Color.WHITE, new TextureAtlas.AtlasRegion(texture, 0, 0, texture.getWidth(), texture.getHeight()), drawX, drawY);
 			
 			return SpireReturn.Return(null);
 		}
 	}
 	
-	@SpirePatch(cls = "com.megacrit.cardcrawl.cards.AbstractCard", method = "renderEnergy")
-	public static class RenderEnergySwitch {
-		public static SpireReturn<?> Prefix(AbstractCard __instance, SpriteBatch sb) {
-			//if it is not a custom card use the default renderer
-			if(!(__instance instanceof CustomCard)) 
-				return SpireReturn.Continue();
-			
-			//if the custom card is cost -2 or locked or not seen or darken end the method
-			if(__instance.cost <= -2 || __instance.isLocked || !__instance.isSeen 
-					|| (boolean) ReflectionHacks.getPrivate(__instance, AbstractCard.class, "darken")) {
-				return SpireReturn.Return(null);
-			}
-			
-			CustomCard card = (CustomCard) __instance;
-			
-			float current_x = (float) ReflectionHacks.getPrivate(__instance, AbstractCard.class, "current_x");
-			float current_y = (float) ReflectionHacks.getPrivate(__instance, AbstractCard.class, "current_y");
-			
-			float drawX = current_x - 256f;
-			float drawY = current_y - 256f;
-
-			Texture texture = card.getOrbSmallTexture();
-			
-			if(texture == null) {
-				Texture baseModTexture = BaseMod.getEnergyOrbTexture(card.color);
-				if (baseModTexture == null) {
-					if(card.color != AbstractCard.CardColor.RED &&
-							card.color != AbstractCard.CardColor.GREEN &&
-							card.color != AbstractCard.CardColor.BLUE &&
-							card.color != AbstractCard.CardColor.COLORLESS &&
-							card.color != AbstractCard.CardColor.CURSE) {
-						texture = ImageMaster.loadImage(BaseMod.getEnergyOrb(card.color));
-						BaseMod.saveEnergyOrbTexture(card.color, texture);
-					} else {
-						switch(card.color) {
-						case BLUE:
-							texture = ImageMaster.CARD_BLUE_ORB;
-							break;
-						case GREEN:
-							texture = ImageMaster.CARD_GREEN_ORB;
-							break;
-						case RED:
-							texture = ImageMaster.CARD_RED_ORB;
-							break;
-						case COLORLESS:
-						case CURSE:
-							texture = ImageMaster.CARD_COLORLESS_ORB;
-							break;
-						}
+	@SpirePatch(
+			clz=AbstractCard.class,
+			method="renderEnergy"
+	)
+	public static class RenderEnergySwitch
+	{
+		public static ExprEditor Instrument()
+		{
+			return new ExprEditor() {
+				@Override
+				public void edit(MethodCall m) throws CannotCompileException
+				{
+					if (m.getClassName().equals(AbstractCard.class.getName()) && m.getMethodName().equals("renderHelper")) {
+						m.replace("{" +
+								"$3 = " + RenderEnergySwitch.class.getName() + ".getEnergyOrb(this, $3);" +
+								"$_ = $proceed($$);" +
+								"}");
 					}
-				} else {
-					texture = baseModTexture;
 				}
-			}
-			
-			renderHelper(card, sb, Color.WHITE.cpy(), texture, drawX, drawY);
-			
-			
-			if(card.type == CardType.STATUS && card.cost == -2) return SpireReturn.Return(null);
-			if(card.type == CardType.CURSE && card.cost == -2) return SpireReturn.Return(null);
-			
-			Color costColor = Color.WHITE.cpy();
-            if (AbstractDungeon.player != null && AbstractDungeon.player.hand.contains(card) && !card.hasEnoughEnergy()) {
-                costColor = (Color) ReflectionHacks.getPrivateStatic(AbstractCard.class, "ENERGY_COST_RESTRICTED_COLOR");
-            } else if(card.isCostModified || card.isCostModifiedForTurn || card.freeToPlayOnce) {
-                costColor = (Color) ReflectionHacks.getPrivateStatic(AbstractCard.class, "ENERGY_COST_MODIFIED_COLOR");
-            }
-			
-			costColor.a = card.transparency;
-			String text = getCost(card);
-			FontHelper.cardEnergyFont_L.getData().setScale(card.drawScale);
-			BitmapFont font = FontHelper.cardEnergyFont_L;
-			
-			FontHelper.renderRotatedText(sb, font, text, current_x, current_y, -132.0f * card.drawScale * Settings.scale, 192.0f * card.drawScale * Settings.scale, card.angle, false, costColor);
-			
-			return SpireReturn.Return(null);
+			};
 		}
-		
-		private static String getCost(AbstractCard card) {
-			if(card.cost == -1)
-				return "X";
-			if(card.freeToPlayOnce)
-				return "0";
-			return Integer.toString(card.costForTurn);
+
+		@SuppressWarnings("unused")
+		public static TextureAtlas.AtlasRegion getEnergyOrb(AbstractCard card, TextureAtlas.AtlasRegion orb)
+		{
+			if (!(card instanceof CustomCard)) {
+				return orb;
+			}
+
+			CustomCard ccard = (CustomCard) card;
+
+			Texture texture = ccard.getOrbSmallTexture();
+			if (texture != null) {
+				return new TextureAtlas.AtlasRegion(texture, 0, 0, texture.getWidth(), texture.getHeight());
+			}
+
+			Texture baseModTexture = BaseMod.getEnergyOrbTexture(card.color);
+			if (baseModTexture != null) {
+				return new TextureAtlas.AtlasRegion(baseModTexture, 0, 0, baseModTexture.getWidth(), baseModTexture.getHeight());
+			}
+
+			switch (card.color) {
+				case BLUE:
+					return ImageMaster.CARD_BLUE_ORB;
+				case GREEN:
+					return ImageMaster.CARD_GREEN_ORB;
+				case RED:
+					return ImageMaster.CARD_RED_ORB;
+				case COLORLESS:
+				case CURSE:
+					return ImageMaster.CARD_COLORLESS_ORB;
+			}
+
+			texture = ImageMaster.loadImage(BaseMod.getEnergyOrb(card.color));
+			BaseMod.saveEnergyOrbTexture(card.color, texture);
+			return new TextureAtlas.AtlasRegion(texture, 0, 0, texture.getWidth(), texture.getHeight());
 		}
 	}
 	
-	@SpirePatch(cls = "com.megacrit.cardcrawl.cards.AbstractCard", method = "renderOuterGlow")
-	public static class RenderOuterGlowSwitch {
+	@SpirePatch(
+			clz=AbstractCard.class,
+			method="renderOuterGlow"
+	)
+	public static class RenderOuterGlowSwitch
+	{
 		public static final Logger logger = LogManager.getLogger(BaseMod.class.getName());
 		
 		@SpireInsertPatch(rloc = 9)
-		public static void Insert(Object __obj_instance, Object sbObj) {
+		public static void Insert(Object __obj_instance, Object sbObj)
+		{
 			AbstractCard card = (AbstractCard) __obj_instance;
 			CardColor color = card.color;
 			SpriteBatch sb = (SpriteBatch) sbObj;
@@ -174,20 +156,28 @@ public class RenderFixSwitches {
 		}
 	}
 	
-	@SpirePatch(cls = "com.megacrit.cardcrawl.cards.AbstractCard", method = "renderCardBg")
-	public static class RenderBgSwitch {
-		public static SpireReturn<?> Prefix(AbstractCard __instance, SpriteBatch sb, float xPos, float yPos) {
-			if(!(__instance instanceof CustomCard) 
+	@SpirePatch(
+			clz=AbstractCard.class,
+			method="renderCardBg"
+	)
+	public static class RenderBgSwitch
+	{
+		public static SpireReturn<?> Prefix(AbstractCard __instance, SpriteBatch sb, float xPos, float yPos)
+		{
+			if (!(__instance instanceof CustomCard)
 					|| __instance.color==AbstractCard.CardColor.RED 
 					|| __instance.color==AbstractCard.CardColor.GREEN
 					|| __instance.color==AbstractCard.CardColor.BLUE 
 					|| __instance.color==AbstractCard.CardColor.COLORLESS
-					|| __instance.color==AbstractCard.CardColor.CURSE) return SpireReturn.Continue();
+					|| __instance.color==AbstractCard.CardColor.CURSE) {
+				return SpireReturn.Continue();
+			}
 			CardColor color = __instance.color;
 			CustomCard card = (CustomCard) __instance;
-			Texture texture;
+			Texture texture = null;
+			TextureAtlas.AtlasRegion region = null;
 			
-			switch(card.type) {
+			switch (card.type) {
 			case POWER:
 				if (BaseMod.getPowerBgTexture(color) == null) {
 					BaseMod.savePowerBgTexture(color, ImageMaster.loadImage(BaseMod.getPowerBg(color)));
@@ -207,46 +197,52 @@ public class RenderFixSwitches {
 				texture = BaseMod.getSkillBgTexture(color);
 				break;
 			default:
-				texture = ImageMaster.CARD_SKILL_BG_BLACK;
+				region = ImageMaster.CARD_SKILL_BG_BLACK;
 				break;
 			}
 			
-			if(!(card.textureBackgroundSmallImg == null) && !card.textureBackgroundSmallImg.isEmpty()) {
+			if (card.textureBackgroundSmallImg != null && !card.textureBackgroundSmallImg.isEmpty()) {
 				texture = card.getBackgroundSmallTexture();
 			}
+
+			if (texture != null) {
+				region = new TextureAtlas.AtlasRegion(texture, 0, 0, texture.getWidth(), texture.getHeight());
+			}
 			
-			if(texture == null) {
+			if (region == null) {
 				BaseMod.logger.info(color.toString() + " texture is null wtf");
 				return SpireReturn.Continue();
 			}
 			
-			renderHelper(card, sb, Color.WHITE, texture, xPos, yPos);
+			renderHelper(card, sb, Color.WHITE, region, xPos, yPos);
 			
 			return SpireReturn.Return(null);
 		}
 	}
 	
-	private static void renderHelper(AbstractCard card, SpriteBatch sb, Color color, Texture texture, float xPos, float yPos) {
+	private static void renderHelper(AbstractCard card, SpriteBatch sb, Color color, TextureAtlas.AtlasRegion region, float xPos, float yPos)
+	{
 		try {
 			// use reflection hacks to invoke renderHelper (without float scale)
 			Method renderHelperMethod;
 			Field renderColorField; 
 			
 			
-			renderHelperMethod = SuperclassFinder.getSuperClassMethod(card.getClass(), "renderHelper", SpriteBatch.class, Color.class, Texture.class, float.class, float.class);
+			renderHelperMethod = SuperclassFinder.getSuperClassMethod(card.getClass(), "renderHelper", SpriteBatch.class, Color.class, TextureAtlas.AtlasRegion.class, float.class, float.class);
 			renderHelperMethod.setAccessible(true);
 			renderColorField = SuperclassFinder.getSuperclassField(card.getClass(), "renderColor");
 			renderColorField.setAccessible(true);
 			
 				
 			Color renderColor = (Color) renderColorField.get(card);
-			renderHelperMethod.invoke(card, sb, renderColor, texture, xPos, yPos);
+			renderHelperMethod.invoke(card, sb, renderColor, region, xPos, yPos);
 		} catch (IllegalAccessException | IllegalArgumentException | NoSuchFieldException | NoSuchMethodException | InvocationTargetException | SecurityException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private static void renderHelper(AbstractCard card, SpriteBatch sb, Color color, Texture texture, float xPos, float yPos, float scale) {
+	private static void renderHelper(AbstractCard card, SpriteBatch sb, Color color, Texture texture, float xPos, float yPos, float scale)
+	{
 		try {
 			// use reflection hacks to invoke renderHelper (without float scale)
 			Method renderHelperMethod;
