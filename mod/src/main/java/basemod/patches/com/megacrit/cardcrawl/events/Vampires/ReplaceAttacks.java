46cf3b91850bf4264a533a8ac3cb2a9ca7fedcc9
Kiooeht <kiooeht@gmail.com>
2019-10-08 18:57:24 -0700

    Make Vampires event use standard remove function

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/events/Vampires/ReplaceAttacks.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/events/Vampires/ReplaceAttacks.java
@@ -2,29 +2,28 @@ package basemod.patches.com.megacrit.cardcrawl.events.Vampires;
 
 import basemod.abstracts.CustomCard;
 import basemod.helpers.BaseModCardTags;
-import basemod.helpers.CardTags;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.city.Vampires;
 
-import java.util.Iterator;
+import java.util.stream.Collectors;
 
 @SpirePatch(
 		clz=Vampires.class,
 		method="replaceAttacks"
 )
 public class ReplaceAttacks {
-	@SpireInsertPatch(rloc=8)
-	public static void Insert(Vampires __instance) {
-		for (Iterator<AbstractCard> i = AbstractDungeon.player.masterDeck.group.iterator(); i.hasNext();) {
-			AbstractCard e = i.next();
-			if (e instanceof CustomCard && ((CustomCard)e).isStrike()) {
-				i.remove();
-			} else if (CardTags.hasTag(e, BaseModCardTags.BASIC_STRIKE)) {
-				i.remove();
-			}
-		}
+	public static void Prefix(Vampires __instance) {
+		AbstractDungeon.player.masterDeck.group.stream()
+				.filter(c -> {
+					if (c instanceof CustomCard && ((CustomCard) c).isStrike()) {
+						return true;
+					} else if (c.hasTag(BaseModCardTags.BASIC_STRIKE)) {
+						return true;
+					}
+					return false;
+				})
+				.collect(Collectors.toList())
+				.forEach(c -> AbstractDungeon.player.masterDeck.removeCard(c));
 	}
 }
