38b46f42cf22b2a8591767bdc26dacd94dcc76da
Adam Volný <adam.volny@gmail.com>
2020-04-09 13:57:56 +0200

    ModLabeledButton - refactor, remove redundant "this.", add multiple constructors for convenience of use

--- mod/src/main/java/basemod/ModLabeledButton.java
+++ mod/src/main/java/basemod/ModLabeledButton.java
@@ -1,7 +1,4 @@
-package basemod;//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
+package basemod;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
@@ -17,18 +14,17 @@ import com.megacrit.cardcrawl.helpers.input.InputHelper;
 import java.util.function.Consumer;
 
 public class ModLabeledButton implements IUIElement {
-    private static final float HB_SHRINK = 14.0F;
     private Consumer<ModLabeledButton> click;
     private Hitbox hb;
-    private Texture texture;
     private float x;
     private float y;
     private float w;
     private float h;
     private String label;
+    private BitmapFont font;
+
     public ModPanel parent;
     public Color color, colorHover;
-    public BitmapFont font;
 
     // this "sinks" the text to the sides - i.e. makes the middle part of button a bit shorter than the text width,
     // looks a bit more balanced to the eye
@@ -37,62 +33,76 @@ public class ModLabeledButton implements IUIElement {
     private Texture textureLeft, textureRight, textureMiddle;
 
     public ModLabeledButton(String label, float xPos, float yPos, ModPanel p, Consumer<ModLabeledButton> c) {
+        this(label, xPos, yPos, Color.WHITE, Color.GREEN, FontHelper.buttonLabelFont, p, c);
+    }
+
+    public ModLabeledButton(String label, float xPos, float yPos, Color textColor, Color textColorHover,
+                            ModPanel p, Consumer<ModLabeledButton> c) {
+        this(label, xPos, yPos, textColor, textColorHover, FontHelper.buttonLabelFont, p, c);
+    }
+
+    public ModLabeledButton(String label, float xPos, float yPos, Color textColor, Color textColorHover,
+                            BitmapFont font, ModPanel p, Consumer<ModLabeledButton> c) {
         this.label = label;
-        this.textureLeft = ImageMaster.loadImage("img/ButtonLeft.png");
-        this.textureRight = ImageMaster.loadImage("img/ButtonRight.png");
-        this.textureMiddle = ImageMaster.loadImage("img/ButtonMiddle.png");
-        this.font = FontHelper.buttonLabelFont;
-        this.x = xPos * Settings.scale;
-        this.y = yPos * Settings.scale;
-        this.w = (this.textureLeft.getWidth() + this.textureRight.getWidth() - TEXT_OFFSET * 2) * Settings.scale +
+        this.font = font;
+        color = textColor;
+        colorHover = textColorHover;
+
+        textureLeft = ImageMaster.loadImage("img/ButtonLeft.png");
+        textureRight = ImageMaster.loadImage("img/ButtonRight.png");
+        textureMiddle = ImageMaster.loadImage("img/ButtonMiddle.png");
+
+        x = xPos * Settings.scale;
+        y = yPos * Settings.scale;
+        w = (this.textureLeft.getWidth() + this.textureRight.getWidth() - TEXT_OFFSET * 2) * Settings.scale +
                 FontHelper.getSmartWidth(font, label, 9999f, 0f);
-        this.h = (float)this.textureLeft.getHeight() * Settings.scale;
-        this.hb = new Hitbox(this.x + 1F * Settings.scale, this.y + 1F * Settings.scale, this.w - 2F * Settings.scale, this.h - 2F * Settings.scale);
-        this.parent = p;
-        this.click = c;
-        this.color = Color.WHITE;
-        this.colorHover = Color.GREEN;
+        h = this.textureLeft.getHeight() * Settings.scale;
+
+        hb = new Hitbox(this.x + 1F * Settings.scale, this.y + 1F * Settings.scale, this.w - 2F * Settings.scale, this.h - 2F * Settings.scale);
+
+        parent = p;
+        click = c;
     }
 
     public void render(SpriteBatch sb) {
-        float text_width = FontHelper.getSmartWidth(this.font, this.label, 9999f, 0.0f) -
+        float text_width = FontHelper.getSmartWidth(font, label, 9999f, 0.0f) -
                 (2 * TEXT_OFFSET) * Settings.scale;
         text_width = Math.max(0, text_width);
 
-        sb.draw(this.textureLeft, this.x, this.y, this.textureLeft.getWidth() * Settings.scale, this.h);
-        sb.draw(this.textureMiddle, this.x + this.textureLeft.getWidth() * Settings.scale, this.y, text_width, this.h);
-        sb.draw(this.textureRight, this.x + this.textureLeft.getWidth() * Settings.scale + text_width, this.y,
-                this.textureRight.getWidth() * Settings.scale, this.h);
+        sb.draw(textureLeft, x, y, textureLeft.getWidth() * Settings.scale, h);
+        sb.draw(textureMiddle, x + textureLeft.getWidth() * Settings.scale, y, text_width, h);
+        sb.draw(textureRight, x + textureLeft.getWidth() * Settings.scale + text_width, y,
+                textureRight.getWidth() * Settings.scale, h);
 
-        this.hb.render(sb);
+        hb.render(sb);
 
         sb.setColor(Color.WHITE);
-        if (this.hb.hovered)
-            FontHelper.renderFontCentered(sb, this.font, this.label, this.hb.cX, this.hb.cY, this.colorHover );
+        if (hb.hovered)
+            FontHelper.renderFontCentered(sb, font, label, hb.cX, hb.cY, colorHover );
         else
-            FontHelper.renderFontCentered(sb, this.font, this.label, this.hb.cX, this.hb.cY, this.color);
+            FontHelper.renderFontCentered(sb, font, label, hb.cX, hb.cY, color);
     }
 
     public void update() {
-        this.hb.update();
-        if (this.hb.justHovered) {
+        hb.update();
+        if (hb.justHovered) {
             CardCrawlGame.sound.playV("UI_HOVER", 0.75F);
         }
 
-        if (this.hb.hovered && InputHelper.justClickedLeft) {
+        if (hb.hovered && InputHelper.justClickedLeft) {
             CardCrawlGame.sound.playA("UI_CLICK_1", -0.1F);
-            this.hb.clickStarted = true;
+            hb.clickStarted = true;
         }
 
-        if (this.hb.clicked) {
-            this.hb.clicked = false;
-            this.onClick();
+        if (hb.clicked) {
+            hb.clicked = false;
+            onClick();
         }
 
     }
 
     private void onClick() {
-        this.click.accept(this);
+        click.accept(this);
     }
 
     public int renderLayer() {
