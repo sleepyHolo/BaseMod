248645a44f35a6b711d798a7307ea4377f569cd2
erasels <emanuelbedburdick@yahoo.de>
2020-12-16 11:05:09 +0100

    Added default methods to IUIElements for positional modification

--- mod/src/main/java/basemod/IUIElement.java
+++ mod/src/main/java/basemod/IUIElement.java
@@ -7,4 +7,9 @@ public interface IUIElement {
 	void update();
 	int renderLayer();
 	int updateOrder();
+	default void move(float xPos, float yPos) {}
+	default void moveX(float xPos) {}
+	default void moveY(float yPos) {}
+	default float getX() {return Integer.MIN_VALUE;}
+	default float getY() {return Integer.MIN_VALUE;}
 }
