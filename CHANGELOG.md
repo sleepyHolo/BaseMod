04225c402699f9e34b14f76661ba88bf0a7f88c9
Kiooeht <kiooeht@gmail.com>
2022-04-22 12:39:49 -0700

    Fix Conspire being unable to load

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -865,3 +865,4 @@ This is a fast forward to v1.6.3 of daviscook477's fork with a few additional ch
 * Remove Note For Yourself patch that is no longer necessary (kiooeht)
 
 #### dev ####
+* Fix Conspire being unable to load (kiooeht)
---
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/events/NoteForYourself/MissingCard.java
@@ -0,0 +1,11 @@
+package basemod.patches.com.megacrit.cardcrawl.events.NoteForYourself;
+
+import com.megacrit.cardcrawl.events.shrines.NoteForYourself;
+
+// This exists purely to avoid a crash with the Conspire mod
+public class MissingCard
+{
+	public static void Replace(NoteForYourself __instance)
+	{
+	}
+}
