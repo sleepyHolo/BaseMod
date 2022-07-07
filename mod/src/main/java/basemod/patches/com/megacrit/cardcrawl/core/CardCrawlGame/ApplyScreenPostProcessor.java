e0fd4f05468eb6e55af82c814df04a4add5a9afa
Kiooeht <kiooeht@gmail.com>
2022-07-06 18:31:14 -0700

    Fix post-processing blend function to not apply alpha again

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/core/CardCrawlGame/ApplyScreenPostProcessor.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/core/CardCrawlGame/ApplyScreenPostProcessor.java
@@ -84,7 +84,7 @@ public class ApplyScreenPostProcessor {
             Gdx.gl.glClearColor(0, 0, 0, 0);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | GL20.GL_STENCIL_BUFFER_BIT);
             sb.begin();
-            sb.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+            sb.setBlendFunction(GL20.GL_ONE, GL20.GL_ZERO);
 
             postProcessor.postProcess(sb, secondaryFboRegion, camera);
 
