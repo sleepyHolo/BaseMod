59ad0f1622b88850a9bd123ac8df3159c389d532
Alchyr <39511935+Alchyr@users.noreply.github.com>
2025-01-17 01:53:43 +0000

    move post initialize call (#437)
    * move post initialize call
    
    * Manually set mode to SPLASH
    
    ---------
    
    Co-authored-by: Kiooeht <kiooeht@gmail.com>

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/core/CardCrawlGame/PostInitializeHook.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/core/CardCrawlGame/PostInitializeHook.java
@@ -1,11 +1,31 @@
 package basemod.patches.com.megacrit.cardcrawl.core.CardCrawlGame;
 
 import basemod.BaseMod;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import javassist.CtBehavior;
 
 @SpirePatch(cls="com.megacrit.cardcrawl.core.CardCrawlGame", method="create")
 public class PostInitializeHook {
     public static void Postfix(Object __obj_instance) {
+        //Leaving for compatibility? Method call is removed, but for a single line method it's unlikely someone relied on its presence
+    }
+
+    @SpireInsertPatch(
+            locator = WithinTryLocator.class
+    )
+    public static void PostInitialize(CardCrawlGame __instance) {
+        // Manually set mode to SPLASH since insert now happens before that gets set in the original method
+        // Not setting it would be unlikely to cause issues, but might as well to be safe
+        CardCrawlGame.mode = CardCrawlGame.GameMode.SPLASH;
         BaseMod.publishPostInitialize();
+    }
+
+    private static class WithinTryLocator extends SpireInsertLocator {
+        @Override
+        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+            Matcher finalMatcher = new Matcher.FieldAccessMatcher(CardCrawlGame.class, "mode");
+            return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+        }
     }
 }
