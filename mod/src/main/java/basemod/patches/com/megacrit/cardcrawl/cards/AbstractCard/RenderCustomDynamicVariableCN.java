8caf51183c8ea2702cdbb734f0ff8192b3aedf17
Chaofan Yang <herbix@users.noreply.github.com>
2025-02-20 12:09:11 +0800

    Fix dynamic variable color in card description when opacity is less than 1. (#440)

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/cards/AbstractCard/RenderCustomDynamicVariableCN.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/cards/AbstractCard/RenderCustomDynamicVariableCN.java
@@ -1,7 +1,9 @@
 package basemod.patches.com.megacrit.cardcrawl.cards.AbstractCard;
 
 import basemod.BaseMod;
+import basemod.ReflectionHacks;
 import basemod.abstracts.DynamicVariable;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
@@ -36,12 +38,19 @@ public class RenderCustomDynamicVariableCN
 			if (dv != null) {
 				if (dv.isModified(__instance)) {
 					if (dv.value(__instance) >= dv.modifiedBaseValue(__instance)) {
-						tmp[0] = "[#" + dv.getIncreasedValueColor().toString() + "]" + Integer.toString(dv.value(__instance)) + "[]";
+						tmp[0] = "[#" + dv.getIncreasedValueColor() + "]" + dv.value(__instance) + "[]";
 					} else {
-						tmp[0] = "[#" + dv.getDecreasedValueColor().toString() + "]" + Integer.toString(dv.value(__instance)) + "[]";
+						tmp[0] = "[#" + dv.getDecreasedValueColor() + "]" + dv.value(__instance) + "[]";
 					}
 				} else {
-					tmp[0] = "[#" + dv.getNormalColor().toString() + "]" + Integer.toString(dv.modifiedBaseValue(__instance)) + "[]";
+					Color textColor = ReflectionHacks.getPrivate(__instance, AbstractCard.class, "textColor");
+					Color dvColor = dv.getNormalColor();
+					float oldAlpha = dvColor.a;
+					if (textColor != null) {
+						dvColor.a = textColor.a;
+					}
+					tmp[0] = "[#" + dvColor + "]" + dv.modifiedBaseValue(__instance) + "[]";
+					dvColor.a = oldAlpha;
 				}
 			}
 		}
